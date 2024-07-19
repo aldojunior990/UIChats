37bf88b6c98ba7efd13b7de6228162d857f7edb4
aldojunior990 <aldojunior990@gmail.com>
2024-07-19 16:45:15 -0300

    first commit

---
+++ src/main/java/com/aldoj/uichats_api/UichatsApiApplication.java
@@ -0,0 +1,13 @@
+package com.aldoj.uichats_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UichatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UichatsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aldoj/uichats_api/UichatsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldoj.uichats_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UichatsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
